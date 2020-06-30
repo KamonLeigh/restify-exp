090c670c82e55379353b365b50f9659fd9c1fe43
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-06-30 17:34:58 +0100

    add user model with login and password hash

---
+++ models/user.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  forename: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('email is invalid');
+      }
+
+      if (!validator.isLength(value, { min: 6, max: undefined })) {
+        throw new Error('password length must be greater than six');
+      }
+    },
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+userSchema.statics.findByCredentials = async (email, password) {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+        throw new Error('Unable to login');
+    }
+    return user
+}
+
+userSchema.pre('save', async (next) => {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
