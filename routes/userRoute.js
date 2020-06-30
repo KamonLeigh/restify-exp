f5d67faab83450262204576936cce54dabc4e728
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-06-30 22:14:02 +0100

    add controller to sign up user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,18 @@
-function user(server) {
+const User = require('../models/user');
 
+function userRouter(server) {
+  server.post('/users', async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+      await user.save();
+      const token = await user.generateToken();
+
+      res.send(201, { user, token });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
 }
 
-module.exports = user;
+module.exports = userRouter;
