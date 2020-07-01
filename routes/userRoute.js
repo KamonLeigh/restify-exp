eac612932bcae3cad23065145cf0f7a6fc6b3188
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-07-01 13:48:09 +0100

    add error-restify

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,17 @@
-// const errors = require('re')
+const errors = require('restify-errors');
 const User = require('../models/user');
 
 function userRouter(server) {
-  server.post('/users', async (req, res) => {
+  server.post('/users', async (req, res, next) => {
     const user = new User(req.body);
 
     try {
       await user.save();
       const token = await user.generateToken();
-
       res.send(201, { user, token });
+      return next();
     } catch (error) {
-      res.send(400, error);
+      return next(new errors.BadRequestError(error));
     }
   });
 }
