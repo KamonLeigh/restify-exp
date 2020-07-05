c349f7e037914f9400038783cad56cfe08e672b7
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-07-05 01:46:35 +0100

    add get all tasks along with get task

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,12 +1,50 @@
+/* eslint-disable no-underscore-dangle */
 const errors = require('restify-errors');
 const Task = require('../models/task');
-const User = require('../models/user');
 const { auth } = require('../middleware');
 
 function taskRouter(server) {
-  server.get('/tasks', auth, (req, res) => {
+  server.get('/tasks', auth, async (req, res, next) => {
+    const match = {};
+    const sort = {};
 
-    //res.send(200, 'hello');
+    if (req.query.completed) {
+      match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+      const path = req.query.sortBy.split(':');
+      sort[path[0]] = path[1] === 'asc' ? 1 : -1;
+    }
+
+    try {
+      await req.user.populate({
+        path: 'tasks',
+        match,
+        sort,
+      }).execPopulate();
+
+      res.send(200, req.user.tasks);
+      return next();
+    } catch (error) {
+      return next(new errors.InternalServerError(error));
+    }
+  });
+
+  server.get('/task/:id', auth, async (req, res, next) => {
+    const _id = req.params.id;
+
+    try {
+      const task = await Task.findOne({ _id, author: req.user._id });
+
+      if (!task) {
+        return next(new errors.NotFoundError('unable to find task'));
+      }
+      res.send(200, task);
+      return next();
+    } catch (error) {
+      return next(new errors.InternalServerError(error));
+    }
   });
 }
 
